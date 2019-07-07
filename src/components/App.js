71e693bbb90effe966842142cbe92273d101e5b5
Carl Hain <crhain@gmail.com>
2019-07-07 10:07:00 -0500

    Update comments in App to better explain how useState hooks are working

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 const App = () => {
   // use useState hook to set state for this functional component
+  // resource - the name we are using for state object
+  // setResource - name for function we are using to set state with
   const [resource, setResource] = useState('posts');
   return (
     <div>
