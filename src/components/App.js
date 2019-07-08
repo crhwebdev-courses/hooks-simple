25d0a87f575546277a1a1e81df8c31dfab75d8ae
Carl Hain <crhain@gmail.com>
2019-07-08 12:49:07 -0500

    Add ResourceLlist component to App and pass resource state to it as a prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
   // use useState hook to set state for this functional component
@@ -11,7 +12,7 @@ const App = () => {
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 };
