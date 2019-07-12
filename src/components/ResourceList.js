69ba27ff80c1eb53dbcade7d62ada133ed88c716
Carl Hain <crhain@gmail.com>
2019-07-12 15:14:44 -0500

    Refactor ResourceList to call function to make api request from within useEffect as an iffy

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -5,18 +5,16 @@ const ResourceList = ({ resource }) => {
   //set state using hook
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async resource => {
-    const response = await Axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
-
   // Note: second argument tells useEffect to rerender (if it changes) - all items
   // in the array will be checked for a change
   useEffect(() => {
-    fetchResource(resource);
+    (async resource => {
+      const response = await Axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+
+      setResources(response.data);
+    })(resource);
   }, [resource]);
 
   return <div>{resources.length}</div>;
