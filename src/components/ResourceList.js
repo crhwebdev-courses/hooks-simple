8ca613351cbd1ba52502483d9e506faa7342f98e
Carl Hain <crhain@gmail.com>
2019-07-12 15:39:16 -0500

    Add title to UserList and ResourceList to differentiate them

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import useResources from './useResources';
+import { directive } from '@babel/types';
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource);
 
   return (
-    <ul>
-      {resources.map(record => (
-        <li key={record.id}>{record.title}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>Resources:</h2>
+      <ul>
+        {resources.map(record => (
+          <li key={record.id}>{record.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,11 +4,14 @@ import useResources from './useResources';
 const UserList = () => {
   const users = useResources('users');
   return (
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>User List:</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
